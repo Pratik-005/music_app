528154b97ca65758c889d9887d0cf8ad0a23d509
Pratik Pisudde <pratik.pisudde.005@gmail.com>
2026-05-13 22:21:24 +0530

    feat:implement functions to play and pause the waveform, chnage the waveform UI

--- client/lib/core/widgets/audio_wave.dart
+++ client/lib/core/widgets/audio_wave.dart
@@ -1,5 +1,7 @@
 import 'package:audio_waveforms/audio_waveforms.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:music_app/core/theme/app_pallate.dart';
 
 class AudioWave extends StatefulWidget {
   const AudioWave({super.key, required this.path});
@@ -12,8 +14,19 @@ class _AudioWaveState extends State<AudioWave> {
 
   final PlayerController prepareContoller = PlayerController();
 
-    void playerInit() async{
+  void playerInit() async{
     await prepareContoller.preparePlayer(path: widget.path);
+  }
+
+  Future<void> playAndPause()async{
+    if(!prepareContoller.playerState.isPlaying){
+      await prepareContoller.startPlayer(forceRefresh: false);
+    }else if (!prepareContoller.playerState.isPaused){
+         await prepareContoller.pausePlayer();
+    }
+    setState(() {
+      
+    });
   }
 
 
@@ -29,11 +42,19 @@ class _AudioWaveState extends State<AudioWave> {
     prepareContoller.dispose();
   }
 
-  
-
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(children:[ 
+      IconButton(onPressed: (){}, icon: prepareContoller.playerState.isPlaying ?  Icon(CupertinoIcons.pause_solid) :  Icon(CupertinoIcons.play_arrow_solid)),
+      Expanded(child: AudioFileWaveforms(
+        playerWaveStyle: PlayerWaveStyle(
+          fixedWaveColor: AppPallate.borderColor,
+          liveWaveColor: AppPallate.gradient2,
+          spacing: 6,
+          showSeekLine: false,
+         
+        ),
+        waveformType: WaveformType.fitWidth,
+        size: Size(double.infinity, 100), playerController: prepareContoller))]);
   }
 }
