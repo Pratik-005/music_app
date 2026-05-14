917978ea691355aa8a9097152f2ef10da4c3b4f6
Pratik Pisudde <pratik.pisudde.005@gmail.com>
2026-05-14 21:47:00 +0530

    feat: create a backend route to upload the song to cloudinary

--- server/main.py
+++ server/main.py
@@ -1,10 +1,18 @@
 import bcrypt
 from fastapi import FastAPI, HTTPException
 from server.models.base import Base
-from server.routes import auth
+from server.routes import auth,song
 from server.db.db import engine
+import cloudinary
+import cloudinary.uploader
 
+cloudinary.config( 
+  cloud_name = "dfztvv4mu", 
+  api_key = "158771665942465", 
+  api_secret = "158771665942465"
+)
 
 Base.metadata.create_all(engine)
 app = FastAPI()
 app.include_router(auth.router,prefix='/auth')
+app.include_router(song.router,prefix='/song')
